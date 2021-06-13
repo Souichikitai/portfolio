76d79cf6e33c65f8f1f2f600d3ab02154d8748d7
souichikitai <souichi.k@icloud.com>
2021-06-13 20:22:18 +0900

    Updates

---
+++ precache-manifest.628e3a36c4d29f4e4732f098bd2c0e55.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45378fce3164fb1db3c846d3a90f0687",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "389f06586104fce3effb",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f868f02cf4d2bfd75c71",
+    "url": "/portfolio/static/css/main.39a9f207.chunk.css"
+  },
+  {
+    "revision": "389f06586104fce3effb",
+    "url": "/portfolio/static/js/2.5440bdc9.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/portfolio/static/js/2.5440bdc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f868f02cf4d2bfd75c71",
+    "url": "/portfolio/static/js/main.d3b9718b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "568b3d8fa3dd8a961626dd39c915d366",
+    "url": "/portfolio/static/media/ProjectReport.568b3d8f.pdf"
+  },
+  {
+    "revision": "d383b192a353f65696cb35309142b79e",
+    "url": "/portfolio/static/media/my_profile_en.d383b192.pdf"
+  },
+  {
+    "revision": "b7f040ce12b9480257b87c9628242d53",
+    "url": "/portfolio/static/media/n10140981CAB230.b7f040ce.pdf"
+  },
+  {
+    "revision": "092fe99c1b2693ab3ceba7b2c26a0150",
+    "url": "/portfolio/static/media/security_analysis.092fe99c.pdf"
+  }
+]);
